4dec7b51970ebdde74a1f9e4cda36b0172203e67
go2023 <tetyanago2018@gmail.com>
2023-09-01 22:56:57 -0700

    Reset and incrementByAmount were added

--- src/features/counter/counterSlice.js
+++ src/features/counter/counterSlice.js
@@ -12,12 +12,22 @@ export const counterSlice = createSlice({
             state.count += 1;
         },
         decrement: (state) => {
+            state.count = 0;
+        },
+        reset: (state) => {
             state.count -= 1;
+        },
+        incrementByAmount: (state, action) => {
+            state.count += action.payload;
         }
     }
 
 });
 
-export const { increment, decrement } = counterSlice.actions;
+export const { increment,
+    decrement ,
+    reset,
+    incrementByAmount,
+} = counterSlice.actions;
 
 export default counterSlice.reducer;
